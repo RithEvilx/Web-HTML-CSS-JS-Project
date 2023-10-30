0b7cc73bc7eca7e885fa0336615adf2860b4cdf0
RithEvil <sovannarith22317@gmail.com>
2023-10-30 20:29:06 +0700

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,4 @@
 let productsHTML = "";
-
 products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
@@ -7,11 +6,9 @@ products.forEach((product) => {
       <img class="product-image"
         src="${product.image}">
     </div>
-
     <div class="product-name limit-text-to-2-lines">
       ${product.name}
     </div>
-
     <div class="product-rating-container">
       <img class="product-rating-stars"
         src="images/ratings/rating-${product.rating.stars * 10}.png">
@@ -19,11 +16,9 @@ products.forEach((product) => {
         ${product.rating.count}
       </div>
     </div>
-
     <div class="product-price">
       $${(product.priceCents / 100).toFixed(2)}
     </div>
-
     <div class="product-quantity-container">
       <select>
         <option selected value="1">1</option>
@@ -38,15 +33,14 @@ products.forEach((product) => {
         <option value="10">10</option>
       </select>
     </div>
-
     <div class="product-spacer"></div>
-
     <div class="added-to-cart">
       <img src="images/icons/checkmark.png">
       Added
     </div>
-
-    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+      product.id
+    }">
       Add to Cart
     </button>
   </div>
@@ -66,7 +60,6 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         matchingItem = item;
       }
     });
-
     if (matchingItem) {
       matchingItem.quantity += 1;
     } else {
@@ -75,6 +68,12 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    // Make Cart Quantity Interactive
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
