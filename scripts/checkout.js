8415ee814248a74b2016373123aec38ca00f3da3
RithEvil <sovannarith22317@gmail.com>
2023-11-04 00:57:39 +0700

    Link All Menu

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -156,10 +156,10 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
     const newQuantity = Number(quantityInput.value);
 
     if (newQuantity < 0 || newQuantity >= 1000) {
-      alert('Quantity must be at least 0 and less than 1000');
+      alert("Quantity must be at least 0 and less than 1000");
       return;
     }
-    
+
     updateQuantity(productId, newQuantity);
 
     const quantityLabel = document.querySelector(
